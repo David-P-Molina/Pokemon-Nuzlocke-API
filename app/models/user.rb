119ff2227c7ad1eab036219cd7046596592d8a26
David Moina <davemolina777@gmail.com>
2021-11-03 12:42:48 -0500

    Ad relationships for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :games
-  has_many :nuzlockes, through: :games
-  has_many :trainers, through: :games
+  has_many :nuzlockes
+  has_many :rules, through: :nuzlocke
+  has_many :trainers
+  has_many :teammates, through: :trainer
+  has_many :pokemon, through: :games
   has_one :graveyard
+  has_many :gravestones, through: :graveyard
 
   def jwt_payload
     super.merge('foo' => 'bar')
