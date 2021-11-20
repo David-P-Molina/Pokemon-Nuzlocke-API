eeb70fcaf9c3840b9fe387089f0cc4da7c556f91
David Moina <davemolina777@gmail.com>
2021-11-19 19:45:53 -0600

    Add fetch notes to poke_info iterator

--- app/models/api_calls/pokemon_api.rb
+++ app/models/api_calls/pokemon_api.rb
@@ -7,7 +7,7 @@ class PokemonAPI
     include HTTParty
 
     def self.fetch_pokemons #fetch first 2 generations, need to make seed info by generation
-        url = "https://pokeapi.co/api/v2/pokemon?limit=251" 
+        url = "https://pokeapi.co/api/v2/pokemon-species?limit=251" 
         pokemons_response = HTTParty.get(url)
         pokemons_list = pokemons_response.body
         pokemons_list
@@ -19,14 +19,16 @@ class PokemonAPI
     def self.fetch_a_pokemon(url)
         poke_response = HTTParty.get(url)
         poke_info = JSON.parse(poke_response.body)
-        poke_info
+        poke_info #retrieve base_happiness,capture_rate,evolution_chain_url,evolves_from_species,
+        #retrieve flavor_text_entries[0](pokedex_entry),generation,habitat,is_baby,is_legendary,is_mythical,
     end
     def self.separate_pokemons
         pokemons_list = self.parse_pokemons
         pokemons_list.each do |poke|
             poke_info = self.fetch_a_pokemon(poke["url"])
             name = poke['name']
-
+            # Use pokedex number to create another fetch call to  https://pokeapi.co/api/v2/pokemon/1
+            #retrieve sprites(back_default, front_default, official-artwork) types, weight, height, abilities,  
             byebug
         end
     end
