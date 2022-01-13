0e3f906f296fc0e9c79aa42c55c9823c71530e70
David Moina <davemolina777@gmail.com>
2022-01-13 12:28:36 -0600

    Create addPlatformToGame migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_015404) do
+ActiveRecord::Schema.define(version: 2022_01_13_182641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_015404) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "generation_id"
+    t.string "platform"
   end
 
   create_table "generations", force: :cascade do |t|
